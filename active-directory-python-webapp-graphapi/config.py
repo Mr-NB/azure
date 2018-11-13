1fff27d3634737d08c251dba8e09d16b9864331d
Mr-NB <15565532751@163.com>
2018-11-13 15:37:17 +0800

    commit

---
+++ active-directory-python-webapp-graphapi/config.py
@@ -0,0 +1,8 @@
+RESOURCE = "https://graph.microsoft.com"  # Add the resource you want the access token for
+TENANT = "microsoft.onmicrosoft.com"  # Enter tenant name, e.g. contoso.onmicrosoft.com
+AUTHORITY_HOST_URL = "https://login.microsoftonline.com"
+CLIENT_ID = "7e903c9f-272c-4a6f-a4e8-ddd4187354f0"  # copy the Application ID of your app from your Azure portal
+CLIENT_SECRET = "tmk2B7FKxjSVCLrTKELmvSvod4ytgBORRieKU0Psens="  # copy the value of key you generated when setting up the application
+
+# These settings are for the Microsoft Graph API Call
+API_VERSION = 'v1.0'
